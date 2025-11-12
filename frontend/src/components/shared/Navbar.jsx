db77b171a5edb507e7dc2c66d6bf5599496cbc7f
Lucky7649 <luckypaswan745@gmail.com>
2025-11-13 00:30:55 +0530

    fix bug

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -9,7 +9,7 @@ import axios from 'axios'
 import { USER_API_END_POINT } from '@/utils/constant'
 import { setUser } from '@/redux/authSlice'
 import { toast } from 'sonner'
-
+import logo from "../../assets/logo.png";
 const Navbar = () => {
     const { user } = useSelector(store => store.auth);
     const dispatch = useDispatch();
@@ -32,7 +32,10 @@ const Navbar = () => {
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
                 <div>
-                    <h1 className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Portal</span></h1>
+                  <h1 className="text-2xl font-bold">
+  <img src={logo} alt="Job Portal Logo" className="h-10 w-auto inline-block" />
+</h1>
+
                 </div>
                 <div className='flex items-center gap-12'>
                     <ul className='flex font-medium items-center gap-5'>
